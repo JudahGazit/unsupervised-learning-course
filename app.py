eaa6b508fb66ca4ac31871b542963b6faa8b8014
Rotem Gazit <gazitrotem@gmail.com>
2020-03-18 19:11:58 +0200

    bugfixes regarding hyperopts

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from IPython.core.display import display
 
 from python_code.learning.clustering.louvain import Louvain
 from python_code.learning.data_loader import load_data
+from python_code.learning.graph.edges_logics.cast_priority_factor import CastPriorityFactor
+from python_code.learning.graph.edges_logics.category import Category
+from python_code.learning.graph.edges_logics.combine import Combine
 from python_code.learning.graph.edges_logics.rating_diff import RatingDiff
 from python_code.learning.graph.graph import create_graph
 from python_code.learning.stats.cluster_stats import *
--- python_code/learning/clustering/optimize_hyperopts.py
+++ python_code/learning/clustering/optimize_hyperopts.py
@@ -1,14 +1,15 @@
-from app import *
+from IPython.core.display import display
+import pandas as pd
 
 RESOLUTIONS = [0.1, 1, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100]
 
 
-def hyper_opts(df, graph, cluster_engine, args_list, result_filename='hyperopts2.csv'):
+def hyper_opts(df, graph, cluster_engine, stats, args_list, result_filename='hyperopts2.csv'):
     stat = []
     for args in args_list:
         display(args)
         partition = cluster_engine(*args).cluster(graph)
         s = stats(df, partition)
-        s['args'] = args
+        s['args'] = ', '.join(map(str, args))
         stat.append(s)
     pd.concat(stat).to_csv(result_filename)
